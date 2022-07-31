71d67ec9a489debeb39bc2764042e7463f0f02be
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-07-30 20:41:46 -0400

    add syntax tree representation classes (borrowed from Finch transpiler)

--- ap/finance.py
+++ ap/finance.py
@@ -12,6 +12,44 @@ class TreeIndenter(Indenter):
     DEDENT_type = '_DEDENT'
     tab_len = 2
 
+class Token:
+    def __init__(self, source, parent=None):
+        self.parent = parent
+        self.source = source
+        for attr in 'type value line column'.split():
+            setattr(self, attr, getattr(self.source, attr))
+            self.length = len(self.source)
+
+    def text(self): return self.value
+
+    def __str__(self):
+        return f'Token <{self.type}, {self.line}:{self.column}> {self.value}'
+
+class Node:
+    def __init__(self, source, parent=None, depth=0, root=None):
+        self.parent = parent
+        self.root = root if root else self
+        self.children = []
+        self.source = source
+
+        self.meta = self.source.meta
+        self.type = self.source.data
+        self.depth = depth
+        for c in self.source.children:
+            #print(c)
+            if isinstance(c, lark.Tree): self.children.append(
+                    Node(c, self, self.depth+1, root=self.root if self.root else self))
+            elif c is None: self.children.append(c)
+            else:
+                assert isinstance(c, lark.Token), c
+                self.children.append(Token(c))
+
+    def text(self):
+        return ''.join(c.text() for c in self.children)
+
+    def __str__(self):
+        return f'Node <{self.type}> ({self.depth})' + '\n' + '\n'.join('  '*self.depth + str(n) for n in self.children)
+
 with open('ledger-grammar.lark', 'r') as grammar:
     parser = Lark(grammar.read(), parser='lalr', lexer='contextual', postlex=TreeIndenter())
     #parser = Lark(grammar.read(), parser='earley', lexer='basic', postlex=TreeIndenter())
@@ -19,3 +57,6 @@ with open('ledger-grammar.lark', 'r') as grammar:
 with open(ledger, 'r') as f:
     parsed = parser.parse(f.read())
 print(parsed.pretty()[:20000])
+tree = Node(parsed)
+print(tree)
+
